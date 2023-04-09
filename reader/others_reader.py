efd66782cb72ee889f121e3873b575ae2989547a
abhay-ahirkar <abhayahirkar2@gmail.com>
2023-04-09 19:35:28 +0530

    made changes

--- reader/others_reader.py
+++ reader/others_reader.py
@@ -2,8 +2,28 @@ import os
 import nibabel as nib
 
 def read_nifti_file(file_path):
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"File not found: {file_path}")
-    img = nib.load(file_path)
-    data = img.get_fdata()
+    # Read the NIFTI file
+    data = nib.load(file_path).get_fdata()
+
+    # Return the data
     return data
+
+def main():
+    # Get the current working directory
+    cwd = os.getcwd()
+
+    # Get the file path
+    file_path = os.path.join(cwd, "data", "fmri.nii.gz")
+
+    # Read the NIFTI file
+    data = read_nifti_file(file_path)
+
+    # Print the data
+    print(data)
+
+if __name__ == "__main__":
+
+    # Run the main function
+    main()
+
+
